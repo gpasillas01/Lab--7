efb9d468dd8ae1d4f9502e28fad2707aecf7cdc4
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 03:50:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+function init(){
 function myFunction() {
   var textInput= document.getElementById("entryinput");
   var textOutput= document.getElementById("textoutput");
-  alert("Griselda Pasillas: " + textInput.value);
+  alert("Desmend Jetton: " + textInput.value);
   textOutput.innerHTML = textInput.value;
   textInput.value="";
 }
   var alertButton =document.getElementById("entrybutton");
   alertButton.addEventListener("click", myFunction);
+
+window.addEventListener('load', init);
